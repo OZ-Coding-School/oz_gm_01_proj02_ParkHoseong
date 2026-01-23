447dd8e2407fc59a46a75314320ceddb153794c0
phs970510-droid <phs970510@gmail.com>
2026-01-23 18:34:23 +0900

    31

---
+++ Project2/Assets/02. Scripts/UI/Pulse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class Pulse : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private float minAlpha = 0.25f;
+    [SerializeField] private float maxAlpha = 1.0f;
+    [SerializeField] private float speed = 6.0f;
+
+    private void Reset()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void Awake()
+    {
+        if (canvasGroup == null) canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void Update()
+    {
+        float t = (Mathf.Sin(Time.time * speed) + 1f) * 0.5f;
+        canvasGroup.alpha = Mathf.Lerp(minAlpha, maxAlpha, t);
+    }
+}
